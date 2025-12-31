a139b77792d9a6a10b83a283dde940e53dcaa716
Saku <ariiyahya3@gmail.com>
2025-12-31 10:13:22 +0300

    fix dist

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,3 +1,8 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
 
-export default defineConfig({});
+export default defineConfig({
+  site: "https://convert-toolss.pages.dev",
+  output: "static",
+  integrations: [sitemap()],
+});
