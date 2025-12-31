2d299e86febad26f7a339f2aece4c0aad2d11328
Saku <ariiyahya3@gmail.com>
2025-12-31 08:44:51 +0300

    Inıtıal commit

---
+++ src/consts.ts
@@ -0,0 +1,11 @@
+const SITE = {
+    name: "Convert Tools",
+    descriptionTR:
+        "Görsellerinizi saniyeler içinde dönüştürün, arka planları temizleyin. Tamamen tarayıcınızda çalışır.",
+    descriptionEN:
+        "Convert images in seconds and remove backgrounds. Everything runs in your browser.",
+    url: "https://YOUR_DOMAIN.com",
+    twitter: "",
+};
+
+export default SITE;
