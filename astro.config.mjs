f72352c1739ec96ee5b8e97d899e8aafe00aa999
Saku <ariiyahya3@gmail.com>
2026-01-06 08:57:39 +0300

    fix astro.config.mjs URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
-  site: "https://convert-toolss.pages.dev",
+  site: "https://www.sakustudios.com.tr",
   output: "static",
   integrations: [sitemap()],
 });
